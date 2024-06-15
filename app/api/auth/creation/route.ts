54beb93b4367936fbb7fcc6f983c3eaad2b45292
Natnael Amin <natnaelamin24@gmail.com>
2024-06-15 13:47:21 +0300

    setting up the setting

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -1,29 +1,43 @@
+
 import prisma from "@/app/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
+import { generateUsername } from "unique-username-generator";
 
 export async function GET(){
     const {getUser} = getKindeServerSession();
     const user = await getUser();
 
-    if (!user || user === null || !user.id){
+  
+    
+    
+     console.log(user);
+    if (!user || user === null || !user.id)
         throw new Error ("something went wrong");
-    }
+    
 
     let dbUser = await prisma.user.findUnique({
         where: {
             id: user.id
         },
-    })
+    });
+
+    
 
     if (!dbUser){
-        dbUser = await prisma.user.create(({
+        dbUser = await prisma.user.create({
         data: {
             id: user.id,
             email: user.email ?? "",
             firstName: user.given_name ?? "",
             lastName: user.family_name ?? "",
             imageUrl: user.picture,
+            userName: generateUsername("-", 3, 12)
         }
-    }))
-    }
+    })
+    } 
+    
+    
+
+    return NextResponse.redirect("http://localhost:3000/")
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
+  console.log("this one should show up  okay!")
   return (
     <p>hey there reddit people</p>
   );
