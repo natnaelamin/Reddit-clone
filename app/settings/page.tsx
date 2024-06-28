44e27d6e34abc4d749e9002b8359cd05bffa310e
Natnael Amin <natnaelamin24@gmail.com>
2024-06-28 23:05:44 +0300

    change username

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,6 +1,7 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import prisma from "../lib/db"
 import { redirect } from "next/navigation";
+import SettingsForm from "@/components/SettingsForm";
 
 async function getData(userId: string){
     const data = await prisma.user.findUnique({
@@ -26,9 +27,9 @@ export default async function Settingspage(){
         }
         const data = await getData(user.id)
         return (
-            <div>
-                <h1>tired of this shit man. i need to see my username asap</h1>
-                {data?.userName}
+            <div className="max-w-[1000px] mx-auto flex-col mt-4">
+                <SettingsForm username={data?.userName}/>
+
             </div>)
         
 }
--- components/SettingsForm.tsx
+++ components/SettingsForm.tsx
@@ -1,9 +1,19 @@
-'use client'
+"use client"
+import { Label } from "./ui/label"
+import { Separator} from "./ui/separator"
+import { Input } from "./ui/input"
 
-function SettingsForm() {
+function SettingsForm({username}:{username: string | null | undefined}) {
   return (
     <form>
       <h1 className="text-3xl font-extrabold tracking-tight">Settings</h1>
+      <Separator className="my-4"/>
+      <Label className="text-lg ">Username</Label>
+      <p className="text-muted-foreground">
+        In this settings page you can change your your username!
+      </p>
+
+      <Input defaultValue={username ?? undefined} name="username" required className="mt-2" min={2} maxLength={21} />
     </form>
   )
 }
