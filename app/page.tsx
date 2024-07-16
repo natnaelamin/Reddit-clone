4771111184375ad90b14414292ef2f7a0faa121c
Natnael Amin <natnaelamin24@gmail.com>
2024-07-16 14:50:31 +0300

    create post card component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import { Card } from "@/components/ui/card";
 import Image from "next/image";
 import Banner from "../public/banner.png"
 import helloImage from "../public/hero-image.png"
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import CreatePostCard from "@/components/CreatePostCard";
+
 
 
 export default function Home() {
@@ -10,7 +15,7 @@ export default function Home() {
   return (
     <div className="max-w-[1000px] mx-auto mt-4 flex gap-x-10">
       <div className="w-[65%] flex flex-col gap-y-5">
-        <p>hey there reddit people</p>
+        <CreatePostCard/>
       </div>
       <div className="w-[35%]">
         <Card>
@@ -27,6 +32,16 @@ export default function Home() {
             <p className="text-sm text-muted-foreground p-2">
               Your Home Reddit frontpage. Come here to check in with your favorite communities!
             </p>
+            <Separator className="my-5"/>
+            <div className="flex flex-col gap-y-5">
+              <Button asChild variant="secondary">
+                <Link href="/r/wakanda/create">Create Post</Link>
+              </Button>
+              <Button asChild>
+                <Link href="/r/create">Create Community</Link>
+                </Button>
+
+            </div>
           </div>
         </Card>
       </div>
